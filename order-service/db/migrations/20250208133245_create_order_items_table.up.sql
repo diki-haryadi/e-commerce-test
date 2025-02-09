ef3d49b59213b01436e4646208013ac226be0f1a
Diki Haryadi <dk.harryadi@gmail.com>
2025-02-09 09:18:54 +0700

    chore: update migration files

--- order-service/db/migrations/20250208133245_create_order_items_table.up.sql
+++ order-service/db/migrations/20250208133245_create_order_items_table.up.sql
@@ -1,14 +1,15 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
+CREATE TYPE stock_status AS ENUM ('reserved', 'deducted', 'released');
 CREATE TABLE order_items (
      "id" uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-     order_id uuid  NULL,
-     product_id uuid   NULL,
-     warehouse_id uuid   NULL,
-     quantity INT NOT NULL,
-     price DECIMAL(15,2) NOT NULL,
-     stock_status ENUM('reserved', 'deducted', 'released') DEFAULT 'reserved',
-     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+     "order_id" uuid  NULL,
+     "product_id" uuid   NULL,
+     "warehouse_id" uuid   NULL,
+     "quantity" INT NOT NULL,
+     "price" DECIMAL(15,2) NOT NULL,
+     "stock_status" stock_status DEFAULT 'released',
+     "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+     "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+     "deleted_at" TIMESTAMP WITH TIME ZONE DEFAULT NULL
 );
 CREATE INDEX idx_order_items_status ON order_items(stock_status);
